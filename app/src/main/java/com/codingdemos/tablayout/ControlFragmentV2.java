c2593acced4964e0fcbe3a5e59b677f761442900
Thanin <5810110140@psu.ac.th>
2020-04-06 21:00:55 +0700

    6-04-63

--- app/src/main/java/com/codingdemos/tablayout/ControlFragmentV2.java
+++ app/src/main/java/com/codingdemos/tablayout/ControlFragmentV2.java
@@ -6,6 +6,7 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.app.NotificationCompat;
 import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
@@ -175,6 +176,7 @@ public class ControlFragmentV2 extends Fragment {
             }
         });
 
+
         return rootview;
     }
 
@@ -187,5 +189,8 @@ public class ControlFragmentV2 extends Fragment {
         }
         return  true;
     }
+
+
+
 
 }
